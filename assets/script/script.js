ff15e4ddac3c6f3815897d5f61409195a74e4cff
cooosmo <cosimo-marasciulo@libero.it>
2024-04-14 19:46:31 +0200

    Implementato codice per pagina valutazione html css e js

--- assets/script/script.js
+++ assets/script/script.js
@@ -233,3 +233,53 @@ nextButton.addEventListener('click', ()=>{
   prossimaDomanda(), clearInterval(timerLoop), startQuizLoop()
 })
 
+
+/* VALUTAZIONE STELLE */
+let stars = document.getElementsByClassName("star");
+
+// Aggiungi event listener per il click su ogni stella
+for (let i = 0; i < stars.length; i++) {
+    stars[i].addEventListener("click", function() {
+        let rating = i + 1;
+        setRating(rating);
+        console.log('sono dentro')
+    });
+
+    // Aggiungi event listener per l'evento mouseover su ogni stella
+    stars[i].addEventListener("mouseover", function() {
+        highlightStars(i + 1);
+    });
+
+    // Aggiungi event listener per l'evento mouseout su ogni stella
+    stars[i].addEventListener("mouseout", function() {
+        removeHighlight();
+    });
+}
+
+function setRating(n) {
+    remove(); 
+
+    for (let i = 0; i < n; i++) {
+        let cls = "star-blue";
+        stars[i].className = "star " + cls;
+    }
+}
+
+function remove() {
+    for (let i = 0; i < stars.length; i++) {
+        stars[i].className = "star";
+    }
+}
+
+function highlightStars(n) {
+    for (let i = 0; i < n; i++) {
+        stars[i].classList.add("star-highlight");
+    }
+}
+
+function removeHighlight() {
+    for (let i = 0; i < stars.length; i++) {
+        stars[i].classList.remove("star-highlight");
+    }
+}
+
